fde56cbdf67cb82edae515b29364ce9858639410
Auliaaaik <auliaimroatulk@students.unnes.ac.id>
2025-06-14 17:29:09 +0700

    Update 1_dashboard.py

--- pages/1_dashboard.py
+++ pages/1_dashboard.py
@@ -2,3 +2,13 @@ import streamlit as st
 
 st.set_page_config(page_title="Prediction")
 st.title("Dashboard")
+
+#Membaca dataset
+df = pd.read_csv("model/Gagal_jantung.csv", sep=';')
+  # Tampilkan dataframe
+    st.subheader("Isi Dataset:")
+    st.dataframe(df)
+
+    # Info ringkas
+    st.subheader("Info Statistik:")
+    st.write(df.describe())

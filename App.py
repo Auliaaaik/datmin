d64f30ea09820f5ce881f62d0af874d6dcc44e3c
Auliaaaik <auliaimroatulk@students.unnes.ac.id>
2025-06-14 17:08:01 +0700

    Update App.py

--- App.py
+++ App.py
@@ -2,4 +2,3 @@ import streamlit as st
 
 st.title("Hello, World!")
 st.write("Ini adalah aplikasi Streamlit pertamaku di GitHub!")
-st.set_page_config(page_title="Iris Dashboard App", layout="Centered")

9df5da3900bbe7cfb3e0340d81c743771078636c
Auliaaaik <auliaimroatulk@students.unnes.ac.id>
2025-06-14 19:29:20 +0700

    Update 3_prediction.py

--- pages/3_prediction.py
+++ pages/3_prediction.py
@@ -3,10 +3,6 @@ import pandas as pd
 import joblib
 
 st.set_page_config(page_title="Prediction")
-st.title("Predictions")
-
-# Konfigurasi halaman
-st.set_page_config(page_title="Prediksi Gagal Jantung", layout="centered")
 st.title("🫀 Prediksi Gagal Jantung Berdasarkan Parameter Klinis")
 
 # Fungsi load model

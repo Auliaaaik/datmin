63931720ee05b15f9fe0feda3e2d0022622554da
Auliaaaik <auliaimroatulk@students.unnes.ac.id>
2025-06-14 17:57:50 +0700

    Update 2_model performance.py

--- pages/2_model performance.py
+++ pages/2_model performance.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 
 # Konfigurasi halaman (hanya sekali, di awal)
 st.set_page_config(page_title="Akurasi Model Random Forest")
